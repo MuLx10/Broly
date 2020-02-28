4b637904dc88a289d4bce656da7d7ce81d39e08d
Mehul Kumar Nirala <mehulkumarnirala@iitkgp.ac.in>
2020-02-28 23:09:13 +0530

    cron task

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 import os, sys
 
 GH_API_KEY = os.getenv('GH_API_KEY')
-BOT_API_KEY = os.getenv('BOT_API_KEY')
-ADMIN_API_KEY = os.getenv('ADMIN_API_KEY')
-
-MATTERMOST_URL = "https://brolee.herokuapp.com"
-
 REDDIT_API_CLIENT_ID = os.getenv('REDDIT_API_CLIENT_ID')
 REDDIT_API_CLIENT_SECRET = os.getenv('REDDIT_API_CLIENT_SECRET')
 REDDIT_API_USER_AGENT = "Meme collector"
 
+BOT_API_KEY = os.getenv('BOT_API_KEY')
+ADMIN_API_KEY = os.getenv('ADMIN_API_KEY')
+
 TEAM_ID = "satzcatq1prftgtfz6c9m5x9my"
+MATTERMOST_URL = "https://brolee.herokuapp.com"
