81c8b2ff3446bb01d5e8da58a87b66997b01c3e1
Mehul Kumar Nirala <mehulkumarnirala@iitkgp.ac.in>
2020-03-02 09:47:00 +0530

    config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os, sys
 # Github API
 GH_API_KEY = os.getenv('GH_API_KEY', '')
 
- # Reddit API
+# Reddit API
 REDDIT_API_CLIENT_ID = os.getenv('REDDIT_API_CLIENT_ID', '') 
 REDDIT_API_CLIENT_SECRET = os.getenv('REDDIT_API_CLIENT_SECRET', '')
 REDDIT_API_USER_AGENT = "Meme collector"
